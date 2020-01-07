9ae4746f9da3f87336c86bfed1d7afd85012cb8c
jlee627 <32377243+jlee627@users.noreply.github.com>
2020-01-07 13:17:42 -0800

    Learning var types

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
     // First Javascript Code! :)
-    console.log('Hello World!');
+    // console.log('Hello World!');
+
+    let firstName = 'Josh';
+    let lastName = 'Lee';
+    console.log(firstName + ' ' + lastName);
+
+    const interestRate = 0.3;
+    interestRate = 1;
+
+    console.log(interestRate);
+
+    // Primitive Types
+    let name = 'Josh'; // String Literal
+    let age = 30; // Number Literal
+    let isApproved = false; // Boolean Literal
+    let firstName = undefined; 
+    let lastName = null;
